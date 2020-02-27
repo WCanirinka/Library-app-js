ac858585407ada81b6f9f1402dc45c50bd9210d0
fegzycole <fergusoniyara@gmail.com>
2020-02-27 23:57:00 +0100

    format document with prettier

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-let myLibrary = [{title: "Grand Entry", author: "Martin", numPages: 45, status: "unread"}];
+let myLibrary = [
+  { title: 'Grand Entry', author: 'Martin', numPages: 45, status: 'unread' }
+];
 
 function Book(author, title, numPages, status) {
-    this.author = author
-    this.title = title;
-    this.numPages = numPages;
-    this.status = status;
+  this.author = author;
+  this.title = title;
+  this.numPages = numPages;
+  this.status = status;
 }
 
-
 function addBookToLibrary(book) {
-    myLibrary.push(book);
-    render();
+  myLibrary.push(book);
+  render();
 }
 
 const app = document.querySelector('#app');
@@ -19,21 +20,20 @@ const bookContainer = document.createElement('div');
 bookContainer.classList.add('book-container');
 
 function render() {
-    myLibrary.forEach((elem, index) => {
-        const bookHtml = `<div class="book-display" id=${index}>
+  myLibrary.forEach((elem, index) => {
+    const bookHtml = `<div class="book-display" id=${index}>
         <h4>${elem.title}</h4>
         <h5>${elem.author}</h5>
         <p>${elem.numPages}</p>
-        <button>${elem.status ? 'unread' : 'read' }</button>
+        <button>${elem.status ? 'unread' : 'read'}</button>
         <button class="delete-btn">Delete</button>
-        </div>`
+        </div>`;
 
-        bookContainer.innerHTML = bookHtml;
-    })
-    app.appendChild(bookContainer);
+    bookContainer.innerHTML = bookHtml;
+  });
+  app.appendChild(bookContainer);
 }
 
 render();
 
-addBookToLibrary(new Book("Mark", "Mercury Node", 60, "read"));
-
+addBookToLibrary(new Book('Mark', 'Mercury Node', 60, 'read'));
